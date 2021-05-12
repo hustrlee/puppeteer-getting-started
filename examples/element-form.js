106cd5bc8ce70ceeb06ec544e0a0fadb3565b749
Ryan Li <hustrlee@gmail.com>
2021-05-12 17:21:47 +0800

    ignore

--- examples/element-form.js
+++ examples/element-form.js
@@ -13,37 +13,40 @@ const data = {
   activityForm: "1. 演员表演\n2. 现场抽奖"
 };
 
-blockPrefix = "#biao-dan-yan-zheng + p + div > div.source > div > form.el-form";
-
-const blocks = {
-  name: `${blockPrefix} > div.el-form-item:nth-child(1)`,
-  location: `${blockPrefix} > div.el-form-item:nth-child(2)`,
-  dateTime: `${blockPrefix} > div.el-form-item:nth-child(3)`,
-  instant: `${blockPrefix} > div.el-form-item:nth-child(4)`,
-  property: `${blockPrefix} > div.el-form-item:nth-child(5)`,
-  resource: `${blockPrefix} > div.el-form-item:nth-child(6)`,
-  activityForm: `${blockPrefix} > div.el-form-item:nth-child(7)`
-};
+const formItems =
+  "#biao-dan-yan-zheng + p + div > div.source > div > form.el-form > div.el-form-item";
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: { width: 1920, height: 1080 }
+  });
 
-  await page.setViewport({ width: 1920, height: 1080 });
+  const page = await browser.newPage();
   await page.goto(url, { waitUntil: "networkidle2" });
 
-  // 模拟输入“活动名称”
-  await page.type(`${blocks.name} input`, data.name);
+  // 找到需要提交的 Form
+  const elFormItems = await page.$$(formItems);
 
-  // 直接设置“活动区域”
-  // await page.$eval(
-  //   `${blocks.location} input`,
-  //   (el, data) => (el.value = data.location),
-  //   data
-  // );
+  // 模拟输入“活动名称”
+  const elTextbox = await elFormItems[0].$("input[type=text]");
+  await elTextbox.type(data.name);
+
   // 模拟设置“活动区域”
-  await page.click(`${blocks.name} input`);
-  await page.waitForSelector("div.el-select-dropdown:not([style*=display])");
+  const elSelect = await elFormItems[1].$("div.el-select");
+  // 模拟点击，等待候选框出现
+  await elSelect.click();
+  const elSelectDropdown = await page.waitForSelector(
+    "div.el-select-dropdown:not([style*=display])"
+  );
+  // 找到候选框中对应项，并点击
+  const elDropdownItems = await elSelectDropdown.$$("ul li");
+  for (let item of elDropdownItems) {
+    let innerText = await item.$eval("span", node => node.innerText);
+    if (innerText === data.location) {
+      await item.click();
+    }
+  }
 
   await page.screenshot({ path: "element-form.png" });
   await browser.close();
